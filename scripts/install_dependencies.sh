e78d820114570466ca6035b36f14abf90c15eed7
maretknow <maretknow.123@gmail.com>
2019-08-21 00:21:38 +0700

    edit dependecy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-if ! [ -x "$(command -v httpd)" ]; then sudo yum amazon-linux-extras install -y httpd php7.2 mysql56-server >&2;   exit 1; fi # install apache if not already installed
+if ! [ -x "$(command -v httpd)" ]; then sudo yum install -y httpd php7.2 mysql56-server >&2;   exit 1; fi # install apache if not already installed
